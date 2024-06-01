685d2f14f6ffacfdb1a585dc8fc660ca5edeab76
tapesh02 <pateltapesh505@gmail.com>
2024-06-01 22:32:44 +0100

    added dashboard screen

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,24 @@
+import { Card, Typography, Stack } from "@mui/material";
+
 const Dashboard = () => {
-  return <h1> Dashboard component</h1>;
+
+  const RenderCards = () => {
+    return Array.from({ length: 5 }, (_, index) => (
+      <Card className="dash-cards" key={index}>
+        <Typography variant="subtitle1">Total Orders</Typography>
+        <Typography variant="h4">{index + 1}</Typography>
+      </Card>
+    ));
+  };
+  return (
+    <div className="dashboard-wrapper">
+      <Typography variant="h6" >
+        Hi! Tapesh
+      </Typography>
+      <Stack direction="column" className="dash-cards-wrapper">
+        <RenderCards/>
+      </Stack>
+    </div>
+  );
 };
 export default Dashboard;
