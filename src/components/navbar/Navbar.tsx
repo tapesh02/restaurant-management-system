a5c102d69a3f7aa5926735c8667f8ce333222513
tapesh02 <pateltapesh505@gmail.com>
2024-06-11 00:07:43 +0100

    fixed bugs and added settings screen

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Box } from "@mui/material";
 import {
@@ -45,13 +45,34 @@ const Navbar = () => {
           key={menu.id}
           role="presentation"
         >
-          <NavLink to={toPath} className="desktop-menu-list" onClick={handleMenu}>
+          <NavLink
+            to={toPath}
+            className="desktop-menu-list"
+            onClick={handleMenu}
+          >
             <span>{menu.icon}</span>
             {menu.menuitem}
           </NavLink>
         </div>
       );
     });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const drawer = document.querySelector<HTMLElement>(".navbar-wrapper")!;
+      if (scrollTop > 5) {
+        drawer.style.top = "14%";
+        drawer.style.height = "calc(100vh - 6.31rem)";
+      } else {
+        drawer.style.top = "12%";
+        drawer.style.height = "calc(100vh - 5.31rem)";
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <Box className="navbar-wrapper">
--- src/components/pages/settings/Settings.tsx
+++ src/components/pages/settings/Settings.tsx
@@ -1,5 +1,36 @@
+import { Button, Slider, Typography } from "@mui/material";
+
 const Settings = () => {
-  return <h1> Settings component</h1>;
+  return (
+    <>
+      <div className="settings-headers">
+        <Typography variant="h5"> Restaurant Settings</Typography>
+        <Typography variant="subtitle1">
+          Manage your restaurants settings and preferences.
+        </Typography>
+      </div>
+      <div className="settings-cards-wrapper">
+        <div className="settings-cards">
+          <div className="card-headers">
+          <Typography variant="h5">Age Requirement</Typography>
+          <Typography variant="subtitle1">
+            Configure the minimum age requirement for customers.
+          </Typography>
+          </div>
+          <div className="cards-content">
+          <Typography variant="body1">
+          Minimum Age Requirement
+          </Typography>
+          <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto"  color="secondary" />
+          <Typography variant="body1">
+          Set the minimum age requirement for customers to enter your restaurant.
+          </Typography>
+          <Button  color="primary" variant="outlined">Save</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Settings;
